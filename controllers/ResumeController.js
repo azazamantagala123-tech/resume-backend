2a1177942cfcdc680eb69901c9f5a4ac1421b07a
Azaz Tagala <azazamantagala123-tech@users.noreply.github.com>
2026-06-19 12:30:37 +0530

    man koni bera bhai

--- controllers/ResumeController.js
+++ controllers/ResumeController.js
@@ -1,4 +1,4 @@
-import Resume from '../models/Resume.js';
+import Resume from '../models/resume.js';
 import AppError from '../utils/AppError.js';
 import catchAsync from '../utils/catchAsync.js';
 
