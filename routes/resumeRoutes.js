5ebab95a4f8e886c08e818e089326de7b8d7b72b
Azaz Tagala <azazamantagala123-tech@users.noreply.github.com>
2026-06-19 12:25:09 +0530

    kuch bhi lekha de

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as resumeController from '../controllers/resumeController.js';
+import * as resumeController from '../controllers/ResumeController';
 
 const router = express.Router();
 
